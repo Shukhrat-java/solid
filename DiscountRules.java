0e9a49ec8362e95230bdfb60abb7cce2df6f19a9
Shukhrat <losteand@yandex.ru>
2025-08-12 18:29:42 +0300

    Create DiscountRules.java
    добавил ещё один осмысленный пример применения принципа избегания магических чисел - систему скидок и лимитов

---
+++ DiscountRules.java
@@ -0,0 +1,11 @@
+public class DiscountRules {
+    // Минимальная сумма для скидки
+    public static final double MIN_AMOUNT_FOR_DISCOUNT = 1000.0;
+    
+    // Размер скидки
+    public static final double DISCOUNT_PERCENTAGE = 0.10;
+    
+    public static final int MAX_ITEM_QUANTITY = 10;
+    
+    private DiscountRules() {}
+}
